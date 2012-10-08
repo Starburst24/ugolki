5e1064995c2b98c752f87b1ffede237d378e8624
Nikita <petergriffin314@gmail.com>
2012-10-08 17:43:28 +0400

    Program carcass

--- ugolkigui.cpp
+++ ugolkigui.cpp
@@ -20,6 +20,15 @@ void UgolkiGUI::drawFrame(UgolkiFrame *frame){
 
 void UgolkiGUI::drawDesk(){
 
+    QBrush black, white;
+    black.setColor(Qt::black);
+    white.setColor(Qt::white);
+    QPen pen;
+    pen.setColor(Qt::red);
+
+    scene->addRect(0,0, 100, 100, pen, black);
+
+
     for (int i = 0; i < DESK_HEIGHT; i++)
         for (int j = 0; j < DESK_WIDTH; j++){
             scene->addRect(DESK_HORIZONTAL_OFFSET_PIXELS +      //Cell start X
@@ -27,8 +36,8 @@ void UgolkiGUI::drawDesk(){
                            DESK_VERTICAL_OFFSET_PIXELS +        //Cell start Y
                            i * DESK_PIXEL_HEIGHT / DESK_HEIGHT,
                            DESK_PIXEL_WIDTH / DESK_WIDTH,       //Cell WIDTH
-                           DESK_PIXEL_HEIGHT / DESK_HEIGHT     //Cell Height
-
+                           DESK_PIXEL_HEIGHT / DESK_HEIGHT,     //Cell Height
+                           pen, (i * DESK_WIDTH + j) % 2 == 0 ? black : white
                           );
         }
 
