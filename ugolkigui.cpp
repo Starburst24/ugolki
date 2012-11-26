48842e96df115d83e415b8a81890d69972d6c775
petergriffin314 <petergriffin314@gmail.com>
2012-11-26 18:46:49 +0000

    changed some size-pixel vars

--- ugolkigui.cpp
+++ ugolkigui.cpp
@@ -29,15 +29,15 @@ void UgolkiGUI::drawDesk(){
     scene->addRect(0,0, 100, 100, pen, black);
 
 
-    for (int i = 0; i < DESK_HEIGHT; i++)
-        for (int j = 0; j < DESK_WIDTH; j++){
+    for (int i = 0; i < DESK_SIZE; i++)
+        for (int j = 0; j < DESK_SIZE; j++){
             scene->addRect(DESK_HORIZONTAL_OFFSET_PIXELS +      //Cell start X
-                           j * DESK_PIXEL_WIDTH / DESK_WIDTH,
+                           j * DESK_SIZE_PIXEL / DESK_SIZE,
                            DESK_VERTICAL_OFFSET_PIXELS +        //Cell start Y
-                           i * DESK_PIXEL_HEIGHT / DESK_HEIGHT,
-                           DESK_PIXEL_WIDTH / DESK_WIDTH,       //Cell WIDTH
-                           DESK_PIXEL_HEIGHT / DESK_HEIGHT,     //Cell Height
-                           pen, (i * DESK_WIDTH + j) % 2 == 0 ? black : white
+                           i * DESK_SIZE_PIXEL / DESK_SIZE,
+                           DESK_SIZE_PIXEL / DESK_SIZE,       //Cell WIDTH
+                           DESK_SIZE_PIXEL / DESK_SIZE,     //Cell Height
+                           pen, (i * DESK_SIZE + j) % 2 == 0 ? black : white
                           );
         }
 
